b7479b4cdeea6a391713026809176542d22d09b3
RevEmmanuel <adeolaae1@gmail.com>
2022-12-08 14:56:59 +0100

    LinkedList

---
+++ nodes.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def prepend(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+        if self.head.next is None:
+            self.tail = self.head
+
+    def append(self, value):
+        new_node = Node(value)
+        self.tail.next = new_node
+        self.tail = new_node
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value, end=" --> ")
+            temp = temp.next
+        print("None")
+
+
+random_node = LinkedList()
+# random_node.prepend(4)
+random_node.append(2)
+random_node.append(4)
+random_node.print_list()
+print(random_node.tail.value)
